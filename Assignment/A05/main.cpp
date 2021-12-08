51871b30972ca33c316c393fae79eab6f161439c
Dymon Browne <70247507+DymonB@users.noreply.github.com>
2021-12-07 19:52:47 -0600

    Update main.cpp

--- Assignment/A05/main.cpp
+++ Assignment/A05/main.cpp
@@ -5,23 +5,82 @@
 * Date      08/29/2021
 * Email     dymonbrowne@gmail.com
 */
-
 #include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
-int main() 
-{
-    int war1,war2 = 0;
-    
-    while (cin >> war1 >> war2) 
-    {
-      if (war1 >= war2)
-      cout << war1 - war2 << endl;
+void T_Format(int Secs);
 
-    else
-      cout << "No Battle!" << endl;
-      
+int main()
+{
+  // Declare the variable to be use.
+  int Time;    
+  cin >> Time;
+  
+  while (Time) 
+  {
+    // vector that holds the series of integers.
+    vector<int> intSeries;
+
+    while (Time)                    
+    {
+      // push back if the integer time is not 0.
+      intSeries.push_back(Time); 
+      // read in time
+      cin >> Time;                        
     }
-    return 0;
+
+    // boolean for the fail to synchronize
+    bool SigFail = true;
+    float Hrs5 = 18000;
+
+    // Find the smallest element in the range then multiple by 2.
+    // if it is less than and equal to 5 hours, find the time
+    // at which all lights synchronize or fail.
+    for (int i = (*min_element(intSeries.begin(), intSeries.end())) * 2; i <= Hrs5; i++)
+    {
+    
+      bool Green = true; 
+
+      // Looping through our vector of integers.
+      for (int j = 0; j < intSeries.size(); j++)
+      {
+        if (i % (intSeries[j] * 2) >= (intSeries[j]) - 5)
+        {
+          // The light is not green Time % 2i !>= i-5
+          Green = false; 
+          break;
+        }
+      }
+        // if We have a green light.
+      if (Green) 
+      {                
+        //the signal is good (bool false) display time
+        SigFail = false;
+        T_Format(i); 
+        break;
+      }
+    }
+    // if this signal fail is true, Print the message
+    if (SigFail) 
+    {
+      cout << "Signals fail to synchronise in 5 hours\n";
+    }
+
+    cin >> Time; 
+    if (!Time)
+    {
+      cin >> Time;
+    }
+  }
+}
+
+void T_Format(int Secs)
+{   // Display the Time in a nice format(HH:MM:SS).
+    cout << setfill('0') << setw(2) << Secs/3600 << ":";
+    cout << setfill('0') << setw(2) << (Secs/60)%60 << ":";
+    cout << setfill('0') << setw(2) << Secs%60 << endl;
 }
